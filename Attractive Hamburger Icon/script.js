9299a93f1ee97b1bbe9fc7c538506f46878feee6
Anshika Jain <anshikajain604@gmail.com>
2020-10-24 14:12:01 +0530

    Add files via upload

---
+++ Attractive Hamburger Icon/script.js
@@ -0,0 +1,5 @@
+function onclickMenu(){
+    document.getElementById("menu").classList.toggle("change");
+    document.getElementById("nav").classList.toggle("change");
+    document.getElementById("menu-bg").classList.toggle("change-bg");
+}
